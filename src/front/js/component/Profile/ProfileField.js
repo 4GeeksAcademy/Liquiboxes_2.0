e3a01f161d1d54c0910d74e73ac446766405706e
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-09-01 11:42:34 +0000

    Perfil Usuario y Registro cambiado el diseño

--- src/front/js/component/Profile/ProfileField.js
+++ src/front/js/component/Profile/ProfileField.js
@@ -7,26 +7,26 @@ export default function ProfileField({ icon, label, value, onEdit, onSave, isEdi
     <div className="card mb-3">
       <div className="card-body">
         <div className="d-flex align-items-center mb-2">
-          <FontAwesomeIcon icon={icon} className="text-primary me-3" />
+          <FontAwesomeIcon icon={icon} className="me-3" />
           <h3 className="card-title mb-0">{label}</h3>
         </div>
         <div className="mt-2">
           {isEditing ? (
-            <div className="d-flex align-items-center">
+            <div className="d-flex justify-content-between align-items-center">
               {children}
               <button
                 onClick={onSave}
-                className="btn btn-primary ms-2"
+                className="ms-2"
               >
                 <FontAwesomeIcon icon={faSave} className="me-1" /> Guardar
               </button>
             </div>
           ) : (
             <div className="d-flex justify-content-between align-items-center">
-              <span className="text-muted">{value}</span>
+              <span className="">{value}</span>
               <button
                 onClick={onEdit}
-                className="btn btn-link text-primary p-0"
+                className="p-1"
               >
                 <FontAwesomeIcon icon={faEdit} className="me-1" /> Editar
               </button>
