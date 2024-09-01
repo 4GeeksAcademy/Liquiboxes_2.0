7b9b24099e3d15516f8c9c28b49c95924c7379b4
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-09-01 12:09:51 +0000

    CarruselTopSellers example

--- src/front/js/component/Home/CarruselTopSellers.js
+++ src/front/js/component/Home/CarruselTopSellers.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import Card_shops from 'Card_shops'
 import Carousel from 'react-bootstrap/Carousel';
 
 
@@ -9,12 +8,11 @@ function CarruselTopSellers() {
       <Carousel fade> {/* Agrega el atributo fade para transiciones suaves */}
         <Carousel.Item>
           <img
-            className="d-block w-100 carousel-image" // Añadir clase para imagen
-            src="https://images.pexels.com/photos/21404298/pexels-photo-21404298/free-photo-of-madera-verano-jardin-casa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="First slide"
-          />
+            className="d-block w-100 carousel-image" 
+            src="https://images.pexels.com/photos/1666070/pexels-photo-1666070.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="First slide"/>          
           <Carousel.Caption>
-            <h3>First slide label</h3>
+            <h3>First Box</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -22,11 +20,10 @@ function CarruselTopSellers() {
         <Carousel.Item>
           <img
             className="d-block w-100 carousel-image"
-            src="https://images.pexels.com/photos/21404298/pexels-photo-21404298/free-photo-of-madera-verano-jardin-casa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Second slide"
-          />
+            src="https://images.pexels.com/photos/697224/pexels-photo-697224.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Second slide"/>          
           <Carousel.Caption>
-            <h3>Second slide label</h3>
+            <h3>Second Box</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
@@ -34,17 +31,15 @@ function CarruselTopSellers() {
         <Carousel.Item>
           <img
             className="d-block w-100 carousel-image"
-            src="https://images.pexels.com/photos/21404298/pexels-photo-21404298/free-photo-of-madera-verano-jardin-casa.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            alt="Third slide"
-          />
+            src="https://images.pexels.com/photos/1050256/pexels-photo-1050256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt="Third slide"/>          
           <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>Third Box</h3>
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
           </Carousel.Caption>
-        </Carousel.Item>
+        </Carousel.Item>          
       </Carousel>
     </div>
   )
 }
-
 export default CarruselTopSellers
