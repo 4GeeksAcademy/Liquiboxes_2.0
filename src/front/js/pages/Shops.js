6908c9191c3528883e221a93b1e6c2d06ce36ff6
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-09-02 17:47:44 +0000

    Botones para cambiar de vista

--- src/front/js/pages/Shops.js
+++ src/front/js/pages/Shops.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Shops() {
+  const [ showArmario, setShowArmario] = useState(false)
+  const [ showValoraciones, setShowValoraciones] = useState(true)
+
+  const handleShowArmario = () => {
+    setShowArmario(true)
+    setShowValoraciones(false)
+  }
+
+  const handleShowValoraciones = () => {
+    setShowArmario(false)
+    setShowValoraciones(true)
+  }
+
+
   return (
-    <div>Shops</div>
+    <div>
+      <button type='button' onClick={handleShowArmario}>Mostrar Armario</button>
+      <button type='button' onClick={handleShowValoraciones}>Mostrar Valoraciones</button>
+
+    {showArmario && (
+      <p>Aquí va el armario</p>
+    )}
+    {showValoraciones && (
+      <p>Aquí va el valoraciones</p>
+    )}
+
+    </div>
   )
 }
 
