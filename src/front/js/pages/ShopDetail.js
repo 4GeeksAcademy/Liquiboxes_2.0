ab1adee9ff0e6218c6af847c49826553187ea154
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-09-03 09:18:55 +0000

    borrado componente CardScroll

--- src/front/js/pages/ShopDetail.js
+++ src/front/js/pages/ShopDetail.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function ShopDetail() {
+  const [isArmarioVisible, setIsArmarioVisible] = useState(false);
+
+  const showArmario = () => setIsArmarioVisible(true);
+  const showValoraciones = () => setIsArmarioVisible(false);
+
   return (
-    <div>ShopDetail</div>
-  )
+
+    <>
+
+      <img src=''></img>
+      <>
+        <div>
+          <h3>Nombre: nombre tienda</h3>
+
+        </div>
+        <div>
+          <button>mensaje</button>
+          <button>seguir</button>
+        </div>
+      </>
+
+
+
+      <div>
+        <button type="button" className='fa-solid fa-table-cells-large' onClick={showArmario}>
+        </button>
+        <button className='fa-solid fa-align-justify' type="button" onClick={showValoraciones}>
+        </button>
+
+        {isArmarioVisible ? (
+          <p>Aquí va el armario</p>
+        ) : (
+          <p>Aquí van las valoraciones</p>
+        )}
+      </div>
+    </>
+
+  );
 }
 
-export default ShopDetail
+export default ShopDetail;
