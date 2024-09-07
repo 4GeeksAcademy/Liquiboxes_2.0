21760d04492354671a4205d74831710a077fff6b
Michell D'Enjoy <166136472+michelldenjoy@users.noreply.github.com>
2024-09-07 23:24:30 +0000

    css cardbox in component CardMBox

--- src/front/js/component/Shop Detail/CardMBox.js
+++ src/front/js/component/Shop Detail/CardMBox.js
@@ -13,23 +13,23 @@ function CardMBox({ data }) {
   };
 
   return (
-    <Card className='container p-3' border="info" style={{ width: '18rem' }}>
-      <Card.Img variant="top" className='card-img'
-        src="https://t4.ftcdn.net/jpg/05/81/14/19/360_F_581141998_4eswgrNT97MJAc2RXm0A9GHGeJ6BX3cb.jpg" alt={data.name} />
-      <Card.Body>
-        {/* Título con el nombre del artículo */}
-        <Card.Title>{data.name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Tienda: {data.shop_name}</Card.Subtitle>
-
-        {/* Información adicional: Precio, ID de tienda, Total de ventas y Categorías */}
-        <Card.Text>
+    <Card className='card-container'>
+      <Card.Img
+        variant="top"
+        className='card-img'
+        src="https://t4.ftcdn.net/jpg/05/81/14/19/360_F_581141998_4eswgrNT97MJAc2RXm0A9GHGeJ6BX3cb.jpg"
+        alt={data.name}
+      />
+      <Card.Body className='card-body'>
+        <Card.Title className='card-title'>{data.name}</Card.Title>
+        <Card.Subtitle className='card-subtitle'>Tienda: {data.shop_name}</Card.Subtitle>
+        <Card.Text className='card-text'>
           Precio: {data.price} <br />
           ID de tienda: {data.shop_id} <br />
           Total de ventas: {data.total_sales} <br />
           Categorías: {data.shop_categories}
         </Card.Text>
-
-        <Button variant="primary" onClick={handleButtonClick}>Check Mystery Box</Button>
+        <Button className='card-button' onClick={handleButtonClick}>Check Mystery Box</Button>
       </Card.Body>
     </Card>
   );
