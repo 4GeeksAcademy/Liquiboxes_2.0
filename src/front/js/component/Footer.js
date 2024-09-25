d57156fce508e8b729acdb4595c7692bd460de93
ElisG24 <160186837+ElisG24@users.noreply.github.com>
2024-09-25 10:57:23 +0000

    se actulizaron varias cosas

--- src/front/js/component/Footer.js
+++ src/front/js/component/Footer.js
@@ -11,15 +11,15 @@ const Footer = () => {
         <div className="row g-4">
           <div className="col-lg-4 col-md-6">
             <h5 className="mb-4">Liquiboxes</h5>
-            <p className="mb-4">Nuestra misión es crear una experiencia de compra emocionante y económica, donde tanto las tiendas como los usuarios salgan ganando. Liquiboxes no solo facilita que los comercios liquiden su inventario, sino que ofrece a los usuarios la oportunidad de obtener productos únicos y a buen precio, todo en un solo click.</p>
+            <p className="mb-4">Déjate sorprender por nuestras Mistery Boxes.</p>
           </div>
           <div className="col-lg-2 col-md-6">
             <h5 className="mb-4">Enlaces Rápidos</h5>
             <ul className="list-unstyled">
-              <li><a href="/about" className="footer-link">Sobre Nosotros</a></li>
-              <li><a href="/services" className="footer-link">Metodos de pago</a></li>
-              <li><a href="/products" className="footer-link">Carrito</a></li>
-              <li><a href="/contact" className="footer-link">Cerrar sesión</a></li>
+              <li><a href="/aboutus" className="footer-link">Sobre Nosotros</a></li>
+              <li><a href="/shopssearch" className="footer-link">Buscador</a></li>
+              <li><a className="footer-link">Cerrar sesión</a></li>
+              <li><a href="/" className="footer-link">Inciar sesión</a></li>
             </ul>
           </div>
           <div className="col-lg-3 col-md-6">
@@ -31,7 +31,7 @@ const Footer = () => {
               </li>
               <li className="mb-2">
                 <FontAwesomeIcon icon={faPhone} className="me-2" />
-                +34 (915) 123-4567
+                +34 915 123 456
               </li>
               <li className="mb-2">
                 <FontAwesomeIcon icon={faEnvelope} className="me-2" />
@@ -42,14 +42,11 @@ const Footer = () => {
           <div className="col-lg-3 col-md-6">
             <h5 className="mb-4">Síguenos</h5>
             <div className="d-flex">
-              <a href="https://facebook.com" target="_blank" rel="noreferrer" className="btn btn-outline-light btn-floating me-2">
+              <a href="https://www.facebook.com/profile.php?id=61565897416494" target="_blank" rel="noreferrer" className="btn btn-outline-light btn-floating me-2">
                 <FontAwesomeIcon icon={faFacebookF} />
               </a>
-              <a href="https://instagram.com" target="_blank" rel="noreferrer" className="btn btn-outline-light btn-floating me-2">
+              <a href="https://www.instagram.com/liquiboxes_esp/" target="_blank" rel="noreferrer" className="btn btn-outline-light btn-floating me-2">
                 <FontAwesomeIcon icon={faInstagram} />
-              </a>
-              <a href="https://linkedin.com" target="_blank" rel="noreferrer" className="btn btn-outline-light btn-floating">
-                <FontAwesomeIcon icon={faLinkedinIn} />
               </a>
             </div>
           </div>
