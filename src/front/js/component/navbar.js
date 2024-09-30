5362f908bce04d3d76ab7e2088c30c683a1dbccf
Miguel Toyas Pernichi <168015510+mitoperni@users.noreply.github.com>
2024-09-30 13:50:13 +0000

    Mejoras UX/UI

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,20 +1,28 @@
-import "../../styles/navbar.css"
-
 import React, { useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Context } from "../store/appContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart, faBars } from "@fortawesome/free-solid-svg-icons";
+import "../../styles/navbar.css"
+
+
 
 export const Navbar = () => {
     const { store, actions } = useContext(Context);
     const [cartItemCount, setCartItemCount] = useState(0);
     const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+    const location = useLocation();
+    const [token, setToken] = useState("")
+    const access_token = sessionStorage.getItem('token') || ""
 
     useEffect(() => {
         const totalItems = Object.values(store.cart).reduce((total, item) => total + item.quantity, 0);
         setCartItemCount(totalItems);
     }, [actions]);
+
+    useEffect(() => {
+        setToken(access_token)
+    }, [access_token])
 
     const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
 
@@ -28,18 +36,19 @@ export const Navbar = () => {
                 <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link to="/aboutus" className="nav-link">Sobre Nosotros</Link>
+                            <Link to="/shopssearch" className={`nav-link ${location.pathname === '/shopssearch' ? 'active' : ''}`}>Buscar Tiendas</Link>
                         </li>
                         <li className="nav-item">
-                            <Link to="/shopssearch" className="nav-link">Buscar Tiendas</Link>
+                            <Link to="/aboutus" className={`nav-link ${location.pathname === '/aboutus' ? 'active' : ''}`}>Sobre Nosotros</Link>
                         </li>
                         <li className="nav-item">
-                            {store.token ? (
-                                <Link to="/userdashboard" className="nav-link">Panel de Control</Link>
+                            {token ? (
+                                <Link to="/userdashboard" className={`nav-link ${location.pathname === '/userdashboard' ? 'active' : ''}`}>Panel de Control</Link>
                             ) : (
-                                <Link to="/" className="nav-link">Iniciar Sesión</Link>
+                                <Link to="/" className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}>Iniciar Sesión</Link>
                             )}
                         </li>
+
                     </ul>
                 </div>
                 <Link to="/cart" className="nav-link position-relative cart-icon">
